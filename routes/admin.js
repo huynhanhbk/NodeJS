cb585da0f4ad387d07125d621bddcd22049d07e0
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-08-16 23:08:33 +0700

    lab4.1 lab4.2 tao controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,25 +1,20 @@
 const path = require('path');
 const express = require('express');
-const rootDir = require('../util/path');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
-
 //EJS
-router.get('/add-product', (req, res, next) => {
-  //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-  });
-});
+// /admin/add-product ==> GET
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+// exports.routes = router;
+// exports.products = products;
+
+//==>
+
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,19 +1,11 @@
 const path = require('path');
 
-const rootDir = require('../util/path');
-
 const express = require('express');
 
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop cua Huynh',
-    path: '/',
-  });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
