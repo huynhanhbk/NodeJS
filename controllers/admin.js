072178831363a8edb84e5bd5e0fb5eae7417d9e6
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-08-21 23:06:31 +0700

    lab 4.14 4.15 dien truoc du lieu vao trang chinh sua sp, Lien ket den trang chinh sua

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,6 +14,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false,
   });
 };
 
@@ -32,12 +33,21 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect('/');
   }
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: editMode,
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product,
+    });
   });
 };
+
+exports.post;
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
