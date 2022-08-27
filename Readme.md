2ac4cfa6fb193f65a032d29342692f42566118e9
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-08-28 00:42:12 +0700

    lab 5.8 ket noi voi co so du lieu sequelize

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
 # Lab 5.1: Kết nối Ứng dụng với Cơ sở dữ liệu SQL
 
 npm install --save mysql2
+
+# Lab 5.8: Kết nối với Cơ sở dữ liệu
+
+npm install --save sequelize
--- util/database.js
+++ util/database.js
@@ -1,10 +1,20 @@
-const mysql = require('mysql2');
+// mysql2
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'node-complete',
+//   password: '12345',
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', '12345', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: '12345',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
