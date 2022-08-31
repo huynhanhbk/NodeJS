16497aa06ebe4b744eb0a8212add6fcf1e99c09b
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-08-31 23:35:07 +0700

    them moi quan he 1-nhieu, adding a one to many relationship

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const errorController = require('./controllers/error');
 //const db = require('./util/database');
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,9 +26,12 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+//xac dinh moi quan he trang buoc, va khi xoa nguoi dung thi viec xoa cung dc thu hien cho sp
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
 sequelize
-  .sync()
+  .sync({ force: true }) //ghi de
   .then((result) => {
     console.log(result);
     app.listen(3000);
