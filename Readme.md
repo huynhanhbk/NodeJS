de8dd05703e7b60bff7aa29ddf6887fd9a8ab096
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-09-10 23:37:46 +0700

    lab5.18 tao va quan ly nguoi dung tam thoi

--- Readme.md
+++ Readme.md
@@ -19,3 +19,14 @@ Lab 5.14: Tìm nạp sản phẩm của quản trị viên
 Lab 5.15: Cập nhật sản phẩm
 Lab 5.16: Xóa sản phẩm
 Lab 5.17: Tạo model người dùng
+Lab 5.18: Tạo và quản lý người dùng tạm thời
+Lab 5.19: Sử dụng phương pháp kết hợp thông minh
+Lab 5.20: Tìm nạp các sản phẩm liên quan
+Lab 5.21: Mối quan hệ một-nhiều & nhiều-nhiều
+Lab 5.22: Tạo & Tìm nạp Giỏ hàng
+Lab 5.23: Thêm sản phẩm mới vào giỏ hàng
+Lab 5.24: Thêm các sản phẩm hiện có & lấy các mặt hàng trong giỏ hàng
+Lab 5.25: Xóa các mặt hàng liên quan & Xóa sản phẩm giỏ hàng
+Lab 5.26: Thêm một model đặt hàng
+Lab 5.27: Lưu trữ mục trong giỏ hàng dưới dạng mục đặt hàng
+Lab 5.28: Đặt lại giỏ hàng, tìm nạp và xuất đơn hàng
--- app.js
+++ app.js
@@ -22,6 +22,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -31,9 +40,23 @@ Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true }) //ghi de
+  // .sync({ force: true }) //ghi de
+  .sync()
   .then((result) => {
-    console.log(result);
+    return User.findById(1);
+    //console.log(result);
+    //app.listen(3000);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user; //lời hứa sẽ giải quyết ngay cho người dùng
+    //Promise.resolve(user): điều này có thể bỏ qua vì nếu bạn trả về 1 return trong then
+    // thì nó sẽ tự động trả về 1 promise mới
+  })
+  .then((user) => {
+    //console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
